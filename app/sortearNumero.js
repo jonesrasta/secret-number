b1798b5dd0a10162442e52d489f1fa2a2a2edc98
Jones Lima <jonesrasta@gmail.com>
2023-05-31 00:10:56 -0300

    validação

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -6,12 +6,10 @@ function gerarNumeroAleatorio() {
   return parseInt(Math.random() * maiorValor + 1);
 }
 
-console.log("Número Secreto", numeroSecreto);
+console.log("Número Secreto:", numeroSecreto);
 
-
-const elementoMenorValor = document.getElementById('menor-valor');
+const elementoMenorValor = document.getElementById("menor-valor");
 elementoMenorValor.innerHTML = menorValor;
 
-
-const elementoMaiorValor = document.getElementById('maior-valor');
+const elementoMaiorValor = document.getElementById("maior-valor");
 elementoMaiorValor.innerHTML = maiorValor;
